21146fef52ff754255e096c023dc0b058ff9bb74
Jonathan Grim <jonjongrim@gmail.com>
2017-07-15 11:24:12 -0400

    Format output of tweets

--- liri.js
+++ liri.js
@@ -1,22 +1,32 @@
 'use strict';
+var chalk = require('chalk');
 var twitterKeys = require('./keys.js');
 var Twitter = require('twitter');
 
-var app = {
-  myTweets: function(count = 20) {
+var [, , fn, mod] = process.argv;
+
+var app = (function() {
+  function myTweets(count = 20) {
     var client = new Twitter({
       consumer_key: twitterKeys.twitter.consumer_key,
       consumer_secret: twitterKeys.twitter.consumer_secret,
       access_token_key: twitterKeys.twitter.access_token_key,
       access_token_secret: twitterKeys.twitter.access_token_secret
     });
+    const log = console.log;
     client.get('search/tweets', { q: 'from:jonjongrim', count: count }, function(error, tweets, response) {
-      console.log(tweets);
+      let msgArray = tweets.statuses;
+      msgArray.forEach(msg => {
+        let [wDay, month, date] = msg.created_at.split(' ');
+        log(chalk.blue(`Tweet at: ${wDay}, ${month} ${date}`));
+        log(`${msg.text}`);
+      });
     });
   }
-};
-
-var [, , fn, mod] = process.argv;
+  return {
+    myTweets: myTweets
+  };
+})();
 
 if (fn === 'my-tweets') {
   app.myTweets();
